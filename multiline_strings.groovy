6f8ea41a3e04decdd7e06d1f6f9fa54346b5b5e9
Victor Hugo <hugo.victoor77@gmail.com>
2025-09-30 06:26:20 -0300

    multine strings slash

--- multiline_strings.groovy
+++ multiline_strings.groovy
@@ -3,4 +3,11 @@ text = """
     How are you doing?
     """
 
+text2 = /
+    Esta é uma string abrangendo
+    várias linhas com barras!
+    Super legal, né?!
+    /
+
 println(text)
+println(text2)
