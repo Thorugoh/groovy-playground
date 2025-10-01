27d5023499efb1fb20c4cc1240bbe3417fd5d4df
Victor Hugo <hugo.victoor77@gmail.com>
2025-10-01 09:10:38 -0300

    create jenkins image

---
+++ jenkins-groovy/local-jenkins/security.groovy
@@ -0,0 +1,21 @@
+#!groovy
+ 
+import jenkins.model.*
+import hudson.security.*
+import jenkins.model.JenkinsLocationConfiguration
+import net.sf.json.JSONObject
+
+def instance = Jenkins.getInstance()
+String admin_email = 'hugo.victoor77@gmail.com'
+JenkinsLocationConfiguration location = instance.getExtensionList('jenkins.model.JenkinsLocationConfiguration')[0]
+ 
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("admin", "admin")
+instance.setSecurityRealm(hudsonRealm)
+ 
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+instance.setAuthorizationStrategy(strategy)
+println "Updating Jenkins Email to: ${admin_email}"
+location.adminAddress = admin_email
+instance.save()
+location.save()
